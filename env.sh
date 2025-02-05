7b22b0ce585abab3ef218075ee09bcf527b79c7c
Scott Beck <scottbeck@gmail.com>
2025-02-05 18:43:52 -0500

    for testing

---
+++ env.sh
@@ -0,0 +1,9 @@
+# for testing purposes
+CODE_RELEASE="latest"
+DEFAULT_WORKSPACE="/workspace"
+SERVER_PORT="8080"
+USER_NAME="coder"
+USER_SHELL="/bin/bash"
+ENABLE_GIT_CONFIG="false"
+GIT_USER=""
+GIT_EMAIL=""
